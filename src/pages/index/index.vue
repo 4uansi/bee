a1b73f52968f08b487feaecf334bdcba2380439e
cangyun <2788998753@qq.com>
2024-10-26 01:15:49 +0800

    🦄 refactor: 完成基础模板

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <view class="text-red-500">hello uniapp</view>
+  <view class="flex justify-center items-center flex-col h-vh">
+    <text class="text-red-500 text-2xl">hello bee!</text>
+  </view>
 </template>
 
 <script setup lang="ts"></script>
