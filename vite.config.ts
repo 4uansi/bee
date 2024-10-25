15534424f95e162e27a0e3949834e90621bc6c15
cangyun <2788998753@qq.com>
2024-10-26 01:08:36 +0800

    ✨ feat: 引入 autoimport

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
@@ -7,6 +8,16 @@ export default defineConfig(async () => {
 
   return {
     envDir: "./env",
-    plugins: [uni(), unocss()],
+    plugins: [
+      uni(),
+      unocss(),
+      AutoImport({
+        imports: ["vue", "uni-app"],
+        dts: "src/types/auto-import.d.ts",
+        dirs: ["src/hooks"],
+        eslintrc: { enabled: true },
+        vueTemplate: true,
+      }),
+    ],
   };
 });
